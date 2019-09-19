5b27d3684893fca7772514be2486cac754161d40
Mike Schon <mschon314@gmail.com>
2019-09-18 22:49:45 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	mux2 "github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+type Message struct {
+	BPM int
+}
+
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+var Blockchain []Block
+
+var mutex = &sync.Mutex{}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		genesisBlock.Hash = calculateHash(genesisBlock)
+		spew.Dump(genesisBlock)
+
+		mutex.Lock()
+		Blockchain = append(Blockchain, genesisBlock)
+		mutex.Unlock()
+	}()
+	log.Fatal(run())
+}
+
+func run() error {
+	mux := makeMuxRouter()
+	httpPort := os.Getenv("PORT")
+	log.Println("HTTP Server Listening on port :", httpPort)
+	s := &http.Server{
+		Addr:           ":" + httpPort,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func makeMuxRouter() http.Handler {
+	muxRouter := mux2.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
+}
+
+func handleGetBlockchain(w http.ResponseWriter, _ *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	write(w, bytes)
+}
+
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&msg); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer func() {
+		if err := r.Body.Close(); err != nil {
+			log.Println("failed to close body")
+		}
+	}()
+
+	mutex.Lock()
+	prevBlock := Blockchain[len(Blockchain)-1]
+
+	newBlock, err := generateBlock(prevBlock, msg.BPM)
+	if err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, msg)
+		return
+	}
+
+	if !isBlockValid(newBlock, prevBlock) {
+		write(w, []byte("block validation failed"))
+
+		return
+	}
+	Blockchain = append(Blockchain, newBlock)
+	spew.Dump(Blockchain)
+
+	mutex.Unlock()
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+func respondWithJSON(w http.ResponseWriter, _ *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		write(w, []byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	write(w, response)
+}
+
+func write(writer http.ResponseWriter, data []byte) {
+	bytesWritten, err := writer.Write(data)
+	if err != nil {
+		log.Printf("failed to write bytes: %s\n", err)
+	} else {
+		log.Printf("wrote %d bytes\n", bytesWritten)
+	}
+}
+
+func calculateHash(block Block) string {
+	record := strconv.Itoa(block.Index) + block.Timestamp + strconv.Itoa(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if calculateHash(oldBlock) != oldBlock.Hash {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
